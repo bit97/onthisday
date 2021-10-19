9783d3732b234c73e6b248878311d847303423f0
Federico Bitondo <federico.bitondo@gmail.com>
2021-10-19 17:04:02 +0200

    update setup

--- setup.py
+++ setup.py
@@ -3,21 +3,19 @@ from setuptools import find_packages, setup
 with open('README.md') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
-
 setup(
     name='onthisday',
     packages=find_packages(include=['onthisday']),
-    version='1.0',
+    version='1.0.1',
     description='Retrieve events that happened on current day',
     long_description=readme,
+    long_description_content_type='text/markdown',
     author='bit97',
     author_email='federico.bitondo@gmail.com',
     url='https://github.com/bit97/onthisday',
     download_url='https://github.com/bit97/onthisday/archive/refs/tags/1.0.tar.gz',
     keywords=['cli', 'command-line', 'wikipedia', 'scraping', 'today-widget', 'today', 'greetings'],
-    license=license,
+    license='WTFPL',
     install_requires=['Babel', 'beautifulsoup4', 'typer'],
     setup_requires=[],
     tests_require=[],
