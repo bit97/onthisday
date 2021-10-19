4138315925648ead4c7e3e8d8118ee1de22e7300
Federico Bitondo <federico.bitondo@gmail.com>
2021-10-19 15:11:44 +0200

    main class and example script

---
+++ main.py
@@ -0,0 +1,3 @@
+from onthisday.today import Today
+
+print(Today('accadde').random(to_date=2001))
--- onthisday/today.py
+++ onthisday/today.py
@@ -1,1 +1,38 @@
+from random import choice
+from typing import List
+from .event import Event
+from .source import Wikipedia, AccaddeOggi
 
+
+class Today:
+    def __init__(self, source_name: str) -> None:
+        source_name = source_name.lower()
+        if 'wiki' in source_name:
+            self.source = Wikipedia()
+        elif 'accadde' in source_name:
+            self.source = AccaddeOggi()
+        else:
+            raise NotImplementedError(f'Unsupported source type {source_name}')
+
+    def _retrieve(self, **kwargs) -> List[Event]:
+        events = self.source.parse()
+
+        if 'from_year' in kwargs:
+            events = [event for event in events if event.get_year() >= kwargs['from_year']]
+
+        if 'to_year' in kwargs:
+            events = [event for event in events if event.get_year() <= kwargs['to_year']]
+
+        return events
+
+    def last(self, n: int = 1) -> List[Event]:
+        events = self._retrieve()
+        return events[slice(abs(n) * -1, None)]
+
+    def random(self, **kwargs) -> Event:
+        events = self._retrieve(**kwargs)
+
+        return choice(events)
+
+    def all(self) -> List[Event]:
+        return self._retrieve()
