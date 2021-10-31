9f4bd971f094d58fceed6d4cf37d4bf84d38e4aa
Federico Bitondo <federico.bitondo@gmail.com>
2021-10-31 11:45:38 +0100

    support locales

--- onthisday/__main__.py
+++ onthisday/__main__.py
@@ -8,9 +8,9 @@ app = Typer()
 
 
 @app.command(name="all")
-def all_(source: str = "wiki") -> None:
+def all_(source: str = "wiki", locale: Optional[str] = None) -> None:
     """Print all the events of today"""
-    event_source = Today(source)
+    event_source = Today(source, locale)
     print(*event_source.all(), sep="\n")
 
 
@@ -18,27 +18,31 @@ def all_(source: str = "wiki") -> None:
 def last_(
     n: int = 1,
     source: str = "wiki",
+    locale: Optional[str] = None,
     from_year: Optional[str] = None,
     to_year: Optional[str] = None,
 ) -> None:
     """Print last n events of today"""
-    event_source = Today(source)
+    event_source = Today(source, locale)
     print(*event_source.last(n, from_year=from_year, to_year=to_year), sep="\n")
 
 
 @app.command(name="random")
 def random_(
-    source: str = "wiki", from_year: Optional[str] = None, to_year: Optional[str] = None
+    source: str = "wiki", locale: Optional[str] = None, from_year: Optional[str] = None, to_year: Optional[str] = None
 ) -> None:
     """Print random event of today"""
-    event_source = Today(source)
+    event_source = Today(source, locale)
     print(event_source.random(from_year=from_year, to_year=to_year))
 
 
 @app.command(name="list")
 def list_() -> None:
     """List the available sources (valid shorter names in brackets)"""
-    available_sources = ["Wikipedia (wiki)", "Accadde Oggi (accadde)"]
+    available_sources = [
+        "Wikipedia (wiki) [with different locales]",
+        "Accadde Oggi (accadde)"
+    ]
     print("\nAvailable sources:\n")
 
     for source in available_sources:
