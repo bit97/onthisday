a5277847342c512e73b0737bd6358eda4e74645e
Federico Bitondo <federico.bitondo@gmail.com>
2021-11-01 20:15:10 +0100

    mock library

---
+++ tests/sources/accadde_oggi/test_accadde_oggi.py
@@ -0,0 +1,9 @@
+import pytest
+from onthisday.event import Event
+from onthisday.source import Source
+from bs4 import BeautifulSoup
+from urllib import request
+
+
+def test_parse():
+    assert False
