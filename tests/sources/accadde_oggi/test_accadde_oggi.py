2fb8ebb3afb2ac45b1af01e91a7ac617b760bb2e
Federico Bitondo <federico.bitondo@gmail.com>
2021-11-02 11:50:56 +0100

    basic Accadde Oggi test

--- tests/sources/accadde_oggi/test_accadde_oggi.py
+++ tests/sources/accadde_oggi/test_accadde_oggi.py
@@ -1,27 +1,22 @@
-import pytest
 from bs4 import BeautifulSoup
-from urllib import request
-import os
 from onthisday.sources.accadde_oggi import AccaddeOggi
 
 
 def test_parse(mocker, events):
-    def load_html(self_unused, url_unused) -> BeautifulSoup:
-        path = os.path.relpath("sources/accadde_oggi/accadde_oggi.html")
-        req = request.Request(f'file://{path}')
-        with request.urlopen(req) as body:  # skipcq: BAN-B310
-            return BeautifulSoup(body, "html.parser")
+
+    def load_html(*_) -> BeautifulSoup:
+        return BeautifulSoup(
+            open("tests/sources/accadde_oggi/accadde_oggi.html"),
+            "html.parser"
+        )
 
     mocker.patch('onthisday.source.Source.get_soup', load_html)
 
     a = AccaddeOggi()
     actual = a.parse()
 
-    # TODO enable test
-
-    #assert len(actual) == len(events)
+    assert len(actual) == len(events)
 
     for idx in range(len(actual)):
-        #assert actual[idx].year == events[idx].year
-        #assert actual[idx].title == events[idx].title
-        pass
+        assert actual[idx].year == events[idx].year
+        # TODO compare titles
