476f04a53513436a12431a869bb3dd3ed8fcba71
Federico Bitondo <federico.bitondo@gmail.com>
2021-11-02 10:06:17 +0100

    relpath

--- tests/sources/accadde_oggi/test_accadde_oggi.py
+++ tests/sources/accadde_oggi/test_accadde_oggi.py
@@ -7,7 +7,7 @@ from onthisday.sources.accadde_oggi import AccaddeOggi
 
 def test_parse(mocker, events):
     def load_html(self_unused, url_unused) -> BeautifulSoup:
-        path = os.path.abspath("sources/accadde_oggi/accadde_oggi.html")
+        path = os.path.relpath("sources/accadde_oggi/accadde_oggi.html")
         req = request.Request(f'file://{path}')
         with request.urlopen(req) as body:  # skipcq: BAN-B310
             return BeautifulSoup(body, "html.parser")
